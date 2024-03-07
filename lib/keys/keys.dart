6a09604287027e133baee46c1b7abfc9d3370f6c
Himanshu kumar <himu2486@gmail.com>
2024-03-08 01:03:35 +0530

    add the new code

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/keys/checkable_todo_items.dart';
 
-import 'package:todo_app/keys/todo_item.dart';
+// import 'package:todo_app/keys/todo_item.dart';
 
 class Todo {
   const Todo(this.text, this.priority);
@@ -69,7 +70,7 @@ class _KeysState extends State<Keys> {
             children: [
               // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
               for (final todo in _orderedTodos)
-                TodoItem(
+                 CheckableTodoItem(
                   todo.text,
                   todo.priority,
                 ),
